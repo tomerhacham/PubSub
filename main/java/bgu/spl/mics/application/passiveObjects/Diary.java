fe8dc2010db192b06f175f15857ddaacbbad1e72
Tomer Hacham <hachamto@post.bgu.ac.il>
2019-12-22 19:31:10 +0200

    Add implementation to Diary need to make it thread safe

--- main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -1,5 +1,6 @@
 package bgu.spl.mics.application.passiveObjects;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -14,10 +15,17 @@ public class Diary {
 
 	//Fields:
 	private static Diary diary=null;
+	private List<Report> reports; //only executed missions will be reported here
+	private int total; //total number of received missions
 	/**
 	 * Retrieves the single instance of this class.
 	 */
-	public Diary(){}
+	private Diary()
+	{
+		reports = new LinkedList<>();
+		total=0;
+
+	}
 
 	public static Diary getInstance() {
 		if(diary==null)
@@ -28,7 +36,7 @@ public class Diary {
 	}
 
 	public List<Report> getReports() {
-		return null;
+		return reports;
 	}
 
 	/**
@@ -36,7 +44,8 @@ public class Diary {
 	 * @param reportToAdd - the report to add
 	 */
 	public void addReport(Report reportToAdd){
-		//TODO: Implement this
+		reports.add(reportToAdd);
+		total++;
 	}
 
 	/**
@@ -55,7 +64,6 @@ public class Diary {
 	 * @return the total number of received missions (executed / aborted) be all the M-instances.
 	 */
 	public int getTotal(){
-		//TODO: Implement this
-		return 0;
+		return total;
 	}
 }
